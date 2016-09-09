61b2cce0b21d051069874cc5c23ae3c7f8690719
Quentin Perez <qperez42@gmail.com>
2016-09-09 03:00:39 +0200

    importe-from-scaleway: remove passwd and motd

--- import-from-scaleway.sh
+++ import-from-scaleway.sh
@@ -7,4 +7,9 @@ then
     curl http://163.172.156.204/x86_64-ubuntu-xenial/rootfs.tar > build/rootfs.tar
 fi
 
- echo "mkdir /media/rootfs && mount -t ext4 /scaleway/disk.img /media/rootfs && tar xpf /scaleway/rootfs.tar -C  /media/rootfs --numeric-owner; sync; umount /media/rootfs" | docker run --privileged --rm -i -v /Users/quentinperez/go/src/github.com/QuentinPerez/busygox/build:/scaleway busygox bash
+echo "mkdir /media/rootfs && mount -t ext4 /scaleway/disk.img /media/rootfs && tar xpf /scaleway/rootfs.tar -C  /media/rootfs --numeric-owner; sync; umount /media/rootfs" | docker run --privileged --rm -i -v `pwd`/build:/scaleway busygox bash
+
+# remove root password
+docker run --rm --privileged -it -v `pwd`/build:/build busygox chroot passwd -d root
+# remove motd to enable the login
+docker run --rm --privileged -it -v `pwd`/build:/build busygox chroot rm /etc/update-motd.d/50-scw
